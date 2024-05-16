6ef7862e2d66c00d8975dd0576daaefc0519be94
teeerapong2020 <teerapong.chue@gmail.com>
2024-05-16 16:46:27 +0700

    back

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ function myFunction() {
       x.style.display = "flex";
     }
   }
+
+
+  // silde show product card
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("show");
+    let dots = document.getElementsByClassName("silde");
+    
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" opacity-100", " opacity-50");
+    }
+    
+    slides[slideIndex-1].style.display = "flex";
+    
+  }
